e61f48993dca3b4a7e7afff489503ff1049531bd
Daniel Macko <macko@gqsystems.eu>
2026-06-11 07:18:39 +0200

    chore: release 3.0.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'danmacko:publish-relations',
   summary: 'Reactive joins for Meteor publications (fork of cottz:publish-relations)',
-  version: '3.0.1',
+  version: '3.0.2',
   git: 'https://github.com/danmacko/meteor-publish-relations',
   documentation: 'README.md'
 });
